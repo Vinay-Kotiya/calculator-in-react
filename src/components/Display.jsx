945214eaebb004f4b715118437c16dfa15a0866e
Vinay_K <vinaykotiya77@gmail.com>
2024-09-17 13:07:20 +0530

    17 sep(Done)

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,6 +1,6 @@
 import styles from './Display.module.css';
 const Display=({displayValue})=>{
-    return<><input type="text" value={displayValue} className={styles.display}  readOnly></input></>;
+    return<><input type="text" value={displayValue} className={styles.length10p}  readOnly></input></>;
     // className={displayValue.length>=9?styles.length10p:styles.display}
 }
 export default Display;
